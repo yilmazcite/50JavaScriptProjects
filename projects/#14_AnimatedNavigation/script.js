06ba45556f0d2161880db64c704b902bc23970fb
Yılmaz Çite <yilmazcite@gmail.com>
2022-04-25 17:26:09 +0300

    14th project added

---
+++ projects/#14_AnimatedNavigation/script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+let toggle = document.getElementById('toggle');
+let nav = document.getElementById('nav');
+
+toggle.addEventListener('click', () => nav.classList.toggle('active'));
