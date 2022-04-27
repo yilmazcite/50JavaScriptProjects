51367716f6903ba62d15226d0f0db18b831cc5ac
Yılmaz Çite <yilmazcite@gmail.com>
2022-04-27 10:53:25 +0300

    16th project

---
+++ projects/#16_DrinkWater/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let smallCups = document.querySelectorAll('.cup-small');
+let liters = document.getElementById('liters');
+let percentage = document.getElementById('percentage');
+let remaining = document.getElementById('remaining');
+
+updateBigCup();
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => highlightCups(index));
+});
+
+function highlightCups(index) {
+  if (
+    smallCups[index].classList.contains('full') &&
+    !smallCups[index].nextElementSibling.classList.contains('full')
+  ) {
+    index--;
+  }
+  smallCups.forEach((cup, idx) => {
+    if (idx <= index) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  let fullCups = document.querySelectorAll('.cup-small.full').length;
+  let totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remaining.style.visibility = 'hidden';
+    remaining.style.height = 0;
+  } else {
+    remaining.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
+}
